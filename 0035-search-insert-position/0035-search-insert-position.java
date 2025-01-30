de66c3579bfa259d59b6102fc51cf0f169d34bd9
ghussein616 <ghussein.616@gmail.com>
2025-01-29 22:20:17 -0600

    Time: 0 ms (100%), Space: 43 MB (65.78%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;  // Target found
+            } else if (nums[mid] < target) {
+                left = mid + 1;  // Search the right half
+            } else {
+                right = mid - 1;  // Search the left half
+            }
+        }
+
+        return left;  // If target is not found, 'left' will be the correct insert position
+    }
+}
